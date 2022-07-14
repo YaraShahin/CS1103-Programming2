d92ae0edd3e670f70cf56e1a274f5944de533d5a
Yara Shahin <58101871+YaraShahin@users.noreply.github.com>
2022-07-14 04:02:06 +0200

    exercise

--- UNIT 3: ADTs, Stacks and Queues/BinarySortTree.java
+++ UNIT 3: ADTs, Stacks and Queues/BinarySortTree.java
@@ -1,45 +1,78 @@
 public class BinarySortTree{
 	
-	private static class TreeNode{
-		String item = null;
-		TreeNode left = null;
-		TreeNode right = null;
+	public static void main(String args[]) {
+		//make the tree
+		BinarySortTree bts = new BinarySortTree();
+		for(int i = 0; i<1023; i++) {
+			int n = (int) (Math.random()*100000);
+			bts.treeInsert(n);
+		}
+		//print the tree for sanity check
+		System.out.println("Tree nodes in order:");
+		bts.treeList(root);
+		System.out.println("--\n\n");	
 		
-		TreeNode(String str){
-			item = str;
+		//testing the three recursive subroutines
+		int count = bts.countLeaves(root);
+		int sum = bts.sumDepth(root, 0);
+		int max = bts.maxDepth(root, 0);
+		System.out.println("Number of leaves: " + count);
+		System.out.println("Sum of leaves' depths: " + sum);
+		System.out.println("Max of leaves' depths: " + max);
+		System.out.println("");
+		
+		System.out.println("The average depth of all the leaves: " + ((double) sum / (double) count));
+		System.out.println("The maximum depth of all the leaves: " + max);
+		
+	}
+	
+	private static TreeNode root = null;
+	private static class TreeNode{
+		int item;
+		TreeNode left;
+		TreeNode right;
+		
+		TreeNode(int n){
+			item = n;
 		}
 	}
-	private static TreeNode root = null;
 	
-	
-	public static void main(String args[]) {
-		BinarySortTree bts = new BinarySortTree();
-		bts.treeInsert("Yara");
-		bts.treeInsert("Nada");
-		//bts.treeInsert("Hadeer");
-		//bts.treeInsert("Mama");
-		//bts.treeInsert("Baba");
-		//bts.treeList(bts.root);
-		System.out.println(bts.treeContains(bts.root, "Yara"));
-
-		System.out.println(bts.treeContains(bts.root, "Nada"));
-		System.out.println(bts.count_nodes(bts.root));
+	static int countLeaves(TreeNode root) {
+		if (root==null) return 0;
+		else if (root.left==null && root.right==null) return 1;
+		else {
+			int count = 0;
+			if (root.left!=null) count += countLeaves(root.left);
+			if (root.right!=null) count += countLeaves(root.right);
+			return count;
+		}
 	}
 	
-	//recursive
-	static boolean treeContains(TreeNode root, String item) {
-		if (root==null) return false;
+	static int sumDepth(TreeNode root, int depth) {
+		if (root==null) return 0;
+		else if (root.left==null && root.right==null) return depth;
 		else {
-			if (item.equals(root.item)) return true;
-			else {
-				if (item.compareTo(root.item)<0) return (treeContains(root.left, item)); 
-				else return (treeContains(root.right, item));
-			}
+			int sum = 0;
+			if (root.left!=null) sum += sumDepth(root.left, depth+1);
+			if (root.right!=null) sum += sumDepth(root.right, depth+1);
+			return sum;
+		}
+	}
+	
+	static int maxDepth(TreeNode root, int depth) {
+		if (root==null) return 0;
+		else if (root.left==null && root.right==null) return depth;
+		else {
+			int leftDepth = 0, rightDepth = 0;
+			if (root.left!=null) leftDepth = maxDepth(root.left, depth+1);
+			if (root.right!=null) rightDepth = maxDepth(root.right, depth+1);
+			if (leftDepth>rightDepth) return leftDepth;
+			else return rightDepth;
 		}
 	}
 	
 	//iterative
-	static void treeInsert(String newItem) {
+	static void treeInsert(int newItem) {
 		TreeNode node = new TreeNode(newItem);
 		if (root==null) {
 			root = node;
@@ -48,7 +81,7 @@ public class BinarySortTree{
 		else {
 			TreeNode runner = root;
 			while (true) {
-				if (newItem.compareTo(runner.item)<0) {
+				if (newItem<runner.item) {
 					if (runner.left==null) {
 						runner.left = node;
 						return;
@@ -80,13 +113,22 @@ public class BinarySortTree{
 	static void treeList(TreeNode root) {
 		if (root==null) return;
 		else {
-			System.out.println(root.item);
 			treeList(root.left);
+			System.out.println(root.item);
 			treeList(root.right);
 			return;
 		}
 	}
 	
-	
-
+	//recursive
+	static boolean treeContains(TreeNode root, int item) {
+			if (root==null) return false;
+			else {
+				if (item == root.item) return true;
+				else {
+					if (item<root.item) return (treeContains(root.left, item)); 
+					else return (treeContains(root.right, item));
+				}
+			}
+		}
 }
